36f6eed21a0053659b45871d0e6db42aae5e22b6
Hoai An Phan <pnhoaian2904@gmail.com>
2022-02-11 07:07:39 +0000

    Completed Function to Track Customer's Payment

--- accounting.py
+++ accounting.py
@@ -1,3 +1,24 @@
+MELON_COST = 1.00
+
+#2 Create function to evaluate if a customer is behind in payments
+def track_customer_payment(name, melon, actual_payment, cost = MELON_COST):
+    ''' Evaluate and print out which customers are overpaid or underpaid
+
+    Inputs:
+    - name of customer
+    - number of melon bought
+    - what he actually paid
+    - the cost of one melon (constant)
+    Output:
+    - print out customer name, what he paid and what he is supposed to pay
+
+    '''
+    
+    expected_payment = melon * cost
+    if expected_payment != actual_payment:
+        print(f"{name} paid ${actual_payment}, expected ${expected_payment}")
+        
+
 #1 Create a function to read text file and save customer's data
 def get_customer_data(path):
     ''' Process the input file and generate customer data report'''
@@ -7,75 +28,14 @@ def get_customer_data(path):
         line = line.rstrip()
         customer_data = line.split('|')
 
-        print(customer_data)
+        # print(customer_data)
 
         customer_name = customer_data[1]
         customer_melon = int(customer_data[2])
         customer_paid = float(customer_data[3])
 
-        
+        track_customer_payment(customer_name, customer_melon, customer_paid)
+
+get_customer_data("customer-orders.txt")
 
 
-# melon_cost = 1.00
-
-# customer1_name = "Joe"
-# customer1_melons = 5
-# customer1_paid = 5.00
-
-# customer2_name = "Frank"
-# customer2_melons = 6
-# customer2_paid = 6.00
-
-# customer3_name = "Sally"
-# customer3_melons = 3
-# customer3_paid = 3.00
-
-# customer4_name = "Sean"
-# customer4_melons = 9
-# customer4_paid = 9.50
-
-# customer5_name = "David"
-# customer5_melons = 4
-# customer5_paid = 4.00
-
-# customer6_name = "Ashley"
-# customer6_melons = 3
-# customer6_paid = 2.00
-
-# #2 Create function to evaluate if a customer is behind in payments
-
-# customer1_expected = customer1_melons * melon_cost
-# if customer1_expected != customer1_paid:
-#     print(f"{customer1_name} paid ${customer1_paid:.2f},",
-#           f"expected ${customer1_expected:.2f}"
-#           )
-
-# customer2_expected = customer2_melons * melon_cost
-# if customer2_expected != customer2_paid:
-#     print(f"{customer2_name} paid ${customer2_paid:.2f},",
-#           f"expected ${customer2_expected:.2f}"
-#           )
-
-# customer3_expected = customer3_melons * melon_cost
-# if customer3_expected != customer3_paid:
-#     print(f"{customer3_name} paid ${customer3_paid:.2f},",
-#           f"expected ${customer3_expected:.2f}"
-#           )
-
-# customer4_expected = customer4_melons * melon_cost
-# if customer4_expected != customer4_paid:
-#     print(f"{customer4_name} paid ${customer4_paid:.2f},",
-#           f"expected ${customer4_expected:.2f}"
-#           )
-
-# customer5_expected = customer5_melons * melon_cost
-# if customer5_expected != customer5_paid:
-#     print(f"{customer5_name} paid ${customer5_paid:.2f},",
-#           f"expected ${customer5_expected:.2f}"
-#           )
-
-# customer6_expected = customer6_melons * melon_cost
-# if customer6_expected != customer6_paid:
-#     print(f"{customer6_name} paid ${customer6_paid:.2f},",
-#           f"expected ${customer6_expected:.2f}"
-#           )
